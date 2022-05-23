73067ce84cfe88e8664ceb9ffbf7e346468f4f68
LH <lh@LHs-MacBook-Air.local>
2022-05-23 21:25:52 +0300

    JavaScript:Commenting code

--- game.js
+++ game.js
@@ -20,6 +20,7 @@ window.onload=function(){
     var end=document.getElementById("end");
     end.onmouseover=reachEnd;
 }
+//function to be exexcuted when player touches one of the boundaries
 function touchBoundary(){
     var bound=document.getElementsByClassName("boundary");
     if(started==true){
@@ -36,12 +37,37 @@ function touchBoundary(){
         document.getElementById("score").innerHTML=score;
     }
 }
+//function to prevent cheating
 function cheating(){
     if(started==true){
         alert("NO CHEATING!!");
         started=false;
     }
 }
+//function to start game and timer when user click on S
+function startPress(){
+    document.getElementById("status").innerHTML="Game On!";
+    time_counter=setInterval(time,100);
+    looser=false;
+    started=true;
+    redBoundaries();
+    document.getElementById("score").innerHTML=score;
+}
+//function to notice when the user reaches the end
+function reachEnd(){
+    //if reached end without game being started this is wrong
+    if(started==true){
+        score+=5;
+        document.getElementById("score").innerHTML=score;
+        document.getElementById("status").innerHTML="You Won!";
+        calculateBestLast();
+        //stop the game to calculate and display score
+        started=false;
+        clearInterval(time_counter);
+        document.getElementById("live").innerHTML=0+":"+0+"."+0;
+    }
+}
+//function to calculate time
 function time(){
     var live=document.getElementById("live").innerHTML;
     var live_splite=live.split(":");
@@ -60,27 +86,7 @@ function time(){
     }
     document.getElementById("live").innerHTML=live_m+":"+live_s+"."+live_ms;
 }
-function startPress(){
-    document.getElementById("status").innerHTML="Game On!";
-    time_counter=setInterval(time,100);
-    looser=false;
-    started=true;
-    redBoundaries();
-    document.getElementById("score").innerHTML=score;
-}
-function reachEnd(){
-    //if reached end without game being started this is wrong
-    if(started==true){
-        score+=5;
-        document.getElementById("score").innerHTML=score;
-        document.getElementById("status").innerHTML="You Won!";
-        calculateBestLast();
-        //stop the game to calculate and display score
-        started=false;
-        clearInterval(time_counter);
-        document.getElementById("live").innerHTML=0+":"+0+"."+0;
-    }
-}
+//function to get best and last time
 function calculateBestLast(){
     var c_time=document.getElementById("live").innerHTML.split(":");
     var current=parseInt(c_time[0])*60+parseFloat(c_time[1]);
@@ -97,6 +103,8 @@ function calculateBestLast(){
     document.getElementById("last").innerHTML=parseInt(max_time/60)+":"+max_time%60;
     document.getElementById("best").innerHTML=parseInt(min_time/60)+":"+min_time%60;
 }
+
+//function to reset on right click
 function reset(){
     started=false;
     score=0;
@@ -104,6 +112,7 @@ function reset(){
     document.getElementById("status").innerHTML="Begin by moving your mouse over the \"S\".";
     
 }
+//function to set boundaries walls to red
 function redBoundaries(){
     var bound=document.getElementsByClassName("boundary");
     for(var i=0;i<bound.length;i++)
